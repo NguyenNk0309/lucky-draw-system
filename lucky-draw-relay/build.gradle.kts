3af457d4eba1b6b7545e8fc6feb9028aafeb2c0d
Khoi Nguyen <khoinguyen.030901@gmail.com>
2026-08-08 13:05:49 +0700

    feat: add lucky draw outbox relay

---
+++ lucky-draw-relay/build.gradle.kts
@@ -0,0 +1,16 @@
+plugins {
+    java
+    id("org.springframework.boot")
+    id("io.spring.dependency-management")
+}
+
+java { toolchain { languageVersion = JavaLanguageVersion.of(17) } }
+
+dependencies {
+    implementation(project(":common-events"))
+    implementation("org.springframework.boot:spring-boot-starter-actuator")
+    implementation("org.springframework.boot:spring-boot-starter-jdbc")
+    implementation("org.springframework.kafka:spring-kafka")
+    runtimeOnly("com.mysql:mysql-connector-j")
+}
+
